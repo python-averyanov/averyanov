0ab086fa55c209d48a07cba8e79fc1bd9f283811
мыш <ggore4045@gmail.com>
2024-11-08 04:40:12 +0300

    Update PZ_6-2.py

--- PZ_6/PZ_6-2.py
+++ PZ_6/PZ_6-2.py
@@ -5,9 +5,8 @@ b = []
 for o in range(1,N):
     a.append(random.randint(1,N))
 
-for i in a:
-    if a[i] > a[i+ 1]:
-        b.append(a)
-
+for i in range(len(a)-1):
+    if a[i] > a[i + 1]:
+        b.append(a[i])
 print(a)
 print(b)
