d1764acd90c065ca528a97c83e621c1f22489f98
python-averyanov <ggore4045@gmail.com>
2025-01-19 21:19:22 +0300

    pz10

---
+++ PZ_10/PZ_10-1.py
@@ -0,0 +1,8 @@
+paterochka  = {"мясо","молоко","сыр"}
+magnit = {"молоко","соль","сахар"}
+a = magnit - paterochka
+print("нет в пятерке ",a)#2
+b = paterochka - magnit
+print("нет в магните ",b)#1
+print("перечень всех товаров: ", paterochka | magnit) #3
+print(magnit is paterochka) #4
