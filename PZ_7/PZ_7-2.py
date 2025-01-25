3b62debe46be76d448af71a0191ab628c02a7f24
python-averyanov <ggore4045@gmail.com>
2025-01-25 14:44:46 +0300

    pz7

--- PZ_7/PZ_7-2.py
+++ PZ_7/PZ_7-2.py
@@ -1,1 +1,18 @@
-"тут пока ничего, жду пояснений"
+ara = ['(',')','[',']','{','}']
+z = []
+for i in ara:
+    a = ord(i)
+    z.append(a)
+print(z)
+def alala(a,b):
+    if a and b in z:
+        None
+    elif a and b not in z:
+        print("скобок нет")
+    elif a and not b in z:
+        print("не хвататет )")
+    elif b and not a in z:
+        print("не хвататет (")
+alala(40,41)
+alala(91,93)
+alala(123,125)
