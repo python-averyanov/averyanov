11646e9de45fcd9918234cde22501d6bb37e3e0b
python-averyanov <ggore4045@gmail.com>
2025-01-30 19:36:53 +0300

    pz14

--- PZ_7/PZ_7-2.py
+++ PZ_7/PZ_7-2.py
@@ -1,1 +1,36 @@
-"тут пока ничего, жду пояснений"
+def check_brackets(s: str) -> int:
+    round = 0
+    square = 0
+    curly = 0
+
+    for i, char in enumerate(s):
+        if char == '(':
+            round += 1
+        elif char == ')':
+            if round == 0:
+                return i + 1
+
+            round -= 1
+        elif char == '[':
+            square += 1
+        elif char == ']':
+            if square == 0:
+                return i + 1
+
+            square -= 1
+        elif char == '{':
+            curly += 1
+        elif char == '}':
+            if curly == 0:
+                return i + 1
+            curly -= 1
+
+    if round > 0 or square > 0 or curly > 0:
+        return -1
+
+    return 0  # Все правильно
+
+
+# Ввод строки
+s = input()
+print(check_brackets(s))
