e482fd6d08229c641fd9440cd6057d370f6ed8dd
python-averyanov <ggore4045@gmail.com>
2025-05-16 03:25:37 +0300

    pz16

---
+++ PZ_14/PZ_14-1.py
@@ -0,0 +1,14 @@
+import re
+with open('writer.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+surnames = re.findall(r'\b[А-ЯЁ][а-яё]+\b', text)
+
+unique= set(surnames)
+
+print(f'Найдено фамилий: {len(surnames)}')
+print(f'Уникальных фамилий: {len(unique)}')
+
+new_text = re.sub(r'\bроман\b', 'произведение', text, flags=re.IGNORECASE)
+
+with open('writer_modified.txt', 'w', encoding='utf-8') as f:
+    f.write(new_text)
