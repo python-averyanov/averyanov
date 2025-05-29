943819c460b1391eb1c201582f003e2cfb46a6cd
python-averyanov <ggore4045@gmail.com>
2025-05-29 19:59:25 +0300

    pz12

--- PZ_13/PZ_13-1.py
+++ PZ_13/PZ_13-1.py
@@ -1,21 +1,14 @@
 # 1. Перенести в новый двумерный список Matr1 элементы, которые не находятся в
 # первых и последних строках и столбцах матрицы Matr2 произвольного размера.
 # 2. В двумерном списке отрицательные элементы возвести в квадрат.
-b = [
-    [1, 2, 3, 4],
-    [5, -6, 7, 8],
-    [-9, 10, 11, 12]
-]
-
-inr = b[1:-1]
+import random
 
 
-a = list(map(lambda row: row[1:-1], inr))
+listy = list(list(random.randint(-10,10) for i in range(4))for i in range(3))
+print(listy)
 
-print(f"новая матрица: {a}")
-a = list(map(
-    lambda r: list(map(lambda x: x**2 if x < 0 else x, r)),
-    a
-))
+inr = listy[1:-1]
+print(inr)
+print('новая матрица: ',a := list(map(lambda row: row[1:-1], inr)))
+print(f'отрицательные элементы в квадрате:', list(map(lambda r: list(map(lambda x: x**2 if x < 0 else x, r)),a)))
 
-print(f'отрицательные элементы в квадрате: {a}')
